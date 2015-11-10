d41aef648baae5a46d10ca8c9d0822e04c1fc94e
Jayson Grace <jayson.e.grace@gmail.com>
2015-11-10 15:20:14 -0700

    Commented code for catfilev.c

--- problem3/catfilev.c
+++ problem3/catfilev.c
@@ -15,47 +15,55 @@
 typedef int bool;
 enum { false, true };
 
-bool DEBUG = false;
+bool DEBUG = true;
 
+/**
+ * @brief Entry into program
+ * @param argc Argument count
+ * @param argv Argument vector
+ * @return Success (or lackthereof) of program's execution
+ */
 int main(int argc, char **argv)
 {
-    if(argc == 1)
+  if(argc == 1)
+  {
+    printf("Usage: %s [directory]\n", *argv);
+    exit(0);
+  }
+
+  struct dirent *file;
+  int inum = atoi(argv[1]);
+  DIR *directory = opendir(argv[2]);
+  while ((file = readdir(directory)) != NULL)
+  {
+    if (DEBUG)
     {
-        printf("Usage: %s [directory]\n", *argv);
-        exit(0);
+      printf("inode %d is for file %s\n", file->d_ino, file->d_name);
     }
 
-    struct dirent *file;
-    int inum = atoi(argv[1]);
-    DIR *directory = opendir(argv[2]);
-    while ((file = readdir(directory)) != NULL)
+    if (file->d_ino == inum)
     {
-        if (DEBUG)
-        {
-            printf("inode %d is for file %s\n",
-                   file->d_ino, file->d_name);
-        }
+      int c;
+      FILE *epicFile;
 
-        if (file->d_ino == inum)
-        {
-            int c;
-            FILE *epicFile;
+      char* filepath;
+      // Add space for the new string
+      filepath = malloc(strlen(argv[2])+1+4);
+      // copy the path of the file into filepath
+      strcpy(filepath, argv[2]);
+      // add the name of the file to filepath
+      strcat(filepath, file->d_name);
 
-            char* filepath;
-            filepath = malloc(strlen(argv[2])+1+4); /* make space for the new string (should check the return value ...) */
-            strcpy(filepath, argv[2]); /* copy name into the new var */
-            strcat(filepath, file->d_name); /* add the extension */
-            
-            epicFile = fopen(filepath, "r");
-            if (epicFile)
-            {
-                while ((c = getc(epicFile)) != EOF)
-                    putchar(c);
-                fclose(epicFile);
-            }
-        }
+      epicFile = fopen(filepath, "r");
+      if (epicFile)
+      {
+        while ((c = getc(epicFile)) != EOF)
+          putchar(c);
+        fclose(epicFile);
+      }
     }
+  }
 
-    closedir(directory);
-    return EXIT_SUCCESS;
+  closedir(directory);
+  return EXIT_SUCCESS;
 }

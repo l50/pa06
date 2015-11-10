307b728b01ba561a0f7afe9d18a7d2ec58a81629
Jayson Grace <jayson.e.grace@gmail.com>
2015-11-10 15:23:16 -0700

    Finished sizewatch.c

--- problem2/sizewatch.c
+++ problem2/sizewatch.c
@@ -12,52 +12,102 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
+#include <syslog.h>
+
+typedef int bool;
+enum { false, true };
+
+FILE * outputLog;
+
+int fprintf(FILE *stream, const char *format, ...);
 
 void getTime()
 {
-    time_t currentTime;
-    struct tm * timeInfo;
-    time(&currentTime);
-    timeInfo = localtime(&currentTime);
-    printf("[%d-%d-%d %d:%02d:%02d] ", (1 + timeInfo->tm_mon), timeInfo->tm_mday,  (1900+timeInfo->tm_year), timeInfo->tm_hour, timeInfo->tm_min, timeInfo->tm_sec);
+  time_t currentTime;
+  struct tm * timeInfo;
+  time(&currentTime);
+  timeInfo = localtime(&currentTime);
+  fprintf(outputLog, "[%d-%d-%d %d:%02d:%02d] ", (1 + timeInfo->tm_mon), timeInfo->tm_mday,  (1900+timeInfo->tm_year), timeInfo->tm_hour, timeInfo->tm_min, timeInfo->tm_sec);
 }
 
+/**
+ * @brief Entry into program
+ * @param argc Argument count
+ * @param argv Argument vector
+ * @return Success (or lackthereof) of program's execution
+ */
 int main(int argc, char* argv[])
 {
-    struct stat fileInfo;
+  struct stat fileInfo;
 
+  pid_t pid, sid;
+
+  // Fork off parent
+  pid = fork();
+
+  // General error check for failure to generate child pid
+  if (pid < 0)
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  // Error check to determine if we can exit parent process
+  if (pid > 0)
+  {
+    exit(EXIT_SUCCESS);
+  }
+
+  // Change file mode mask
+  umask(0);
+
+  // Open output log
+  outputLog = fopen ("output.txt", "w+");
+
+  // Create new SID for child process
+  sid = setsid();
+  if (sid < 0)
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  if(stat(argv[1], &fileInfo) == -1)
+  {
+    fprintf(outputLog, "Error: Input file not found!\n");
+    return -1;
+  }
+
+  getTime();
+  fprintf(outputLog, "Monitoring File: %s\n", argv[1]);
+
+  off_t fileSize;
+  fileSize = fileInfo.st_size;
+  int i;
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  getTime();
+  fprintf(outputLog, "Initial Size: %d\n", (int) fileSize);
+
+  for(i = 0; i < 18; i++)
+  {
     if(stat(argv[1], &fileInfo) == -1)
     {
-        printf("Error: Input file not found!\n");
-        return -1;
+      fprintf(outputLog, "Error: The file has been moved or deleted!\n");
+      return -1;
     }
-
-    getTime();
-    printf("Monitoring File: %s\n", argv[1]);
-
-    off_t fileSize;
-    fileSize = fileInfo.st_size;
-    int i;
-
-    getTime();
-    printf("Initial Size: %d\n", (int) fileSize);
-
-    for(i = 0; i < 18; i++)
+    if((fileSize != fileInfo.st_size))
     {
-        if(stat(argv[1], &fileInfo) == -1)
-        {
-            printf("Error: The file has been moved or deleted!\n");
-            return -1;
-        }
-        if((fileSize != fileInfo.st_size))
-        {
-            getTime();
-            printf("Size Changed: Old: %d, New = %d\n", (int)fileSize, (int)fileInfo.st_size);
-            fileSize = fileInfo.st_size;
-        }
-        sleep(10);
+      getTime();
+      fprintf(outputLog, "Size Changed: Old: %d, New = %d\n", (int)fileSize, (int)fileInfo.st_size);
+      fileSize = fileInfo.st_size;
     }
+    sleep(10);
+  }
 
-    getTime();
-    printf("Monitoring File terminated, BYE.\n");
+  getTime();
+  fprintf(outputLog, "Monitoring File terminated, BYE.\n");
+  fclose(outputLog);
+  return EXIT_SUCCESS;
 }
